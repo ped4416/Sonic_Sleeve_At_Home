35b3214f4721fb64001145304ff9259c5bb810e5
Bryan Dunphy <bryandunphy@gmail.com>
2020-11-02 17:57:34 +0000

    virtual button changes colour on collision

---
+++ Assets/ChangeBoxColour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBoxColour : MonoBehaviour
+{
+    private Material mat;
+    private Color col = new Color();
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = Color.red;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+        mat.color = col;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+        mat.color = Color.white;
+    }
+}
