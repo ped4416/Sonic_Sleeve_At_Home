c822beb6aa435daa5799ac6634aa10b051841e41
Bryan Dunphy <bryandunphy@gmail.com>
2020-11-03 18:37:24 +0000

    created InteractIML_Interface script to control interactml nodes

---
+++ Assets/InteractML_Interface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+public class InteractML_Interface : MonoBehaviour
+{
+
+    public IMLComponent model;
+    public IMLTrainingExamplesUI trainingNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("r"))
+        {
+            //find tran the machine node
+          //model.MLController.nodes.
+        }
+    }
+}
