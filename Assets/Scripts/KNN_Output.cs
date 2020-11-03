0a188845572e372e54339ba9ccee39582d10f594
Bryan Dunphy <bryandunphy@gmail.com>
2020-11-03 15:44:17 +0000

    added knn left and right models, shoulder abduction left model

---
+++ Assets/Scripts/KNN_Output.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+public class KNN_Output : MonoBehaviour
+{
+    [PullFromIMLController]
+    public int i_knnOutputValue;
+
+    private int i_prevVal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i_prevVal = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i_currentVal;
+
+        i_currentVal = i_knnOutputValue;
+        if (i_currentVal != i_prevVal) print("KNN Output = " + i_currentVal);
+        i_prevVal = i_currentVal;
+    }
+}
