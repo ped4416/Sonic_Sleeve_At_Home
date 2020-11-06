d95e847f07824ad5dd016732d83537dcb071db87
Bryan Dunphy <bryandunphy@gmail.com>
2020-11-06 11:48:00 +0000

    added 2 minute timer

---
+++ Assets/TwoMinTimer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoMinTimer : MonoBehaviour
+{
+    //public OnTenRepsListener tenRepsListener;
+    //public On10SecTimerEnd timerEnd;
+
+    private int timer;
+    private bool timerOn;
+
+    private void Start()
+    {
+        timer = 0;
+        timerOn = false;
+    }
+
+    public void Begin()
+    {
+        print("2 minute timer start!");
+        timerOn = true;
+        StartCoroutine(TwoMinCoroutine());
+
+    }
+
+    IEnumerator TwoMinCoroutine()
+    {
+        while (timerOn)
+        {
+            increaseTime();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private void increaseTime()
+    {
+        timer += 1;
+        print("Time in seconds: " + timer);
+        if (timer == 120)
+        {
+            timer = 0;
+            End();
+        }
+    }
+
+    public void End()
+    {
+        timerOn = false;
+        StopCoroutine(TwoMinCoroutine());
+        print("Check Start Position!");
+        //timerEnd.Raise();
+    }
+}
