c6cc64797a2ae797c7bc33ab839524715421cb80
ped4416 <ped4416@gmail.com>
2020-11-13 13:21:35 +0000

    added an audio load from file script

---
+++ Assets/Scripts/Events/AudioController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AudioController : MonoBehaviour
+{
+    public const string audioName = "song.wav";
+
+    [Header("Audio Stuff")]
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public string soundPath;
+
+    private void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        //filepath needs to work on multiple machines. To do!
+        soundPath = "C://Users//UCLH_RehabKit_12//Documents//Unity Projects//samples";
+        Debug.Log("Loading file");
+        StartCoroutine(GetAudioClip());
+    }
+
+    IEnumerator GetAudioClip()
+    {
+        using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(soundPath + "//song.wav", AudioType.WAV))
+        {
+
+            yield return uwr.SendWebRequest();
+
+            //may need a pause here? 
+            audioClip = DownloadHandlerAudioClip.GetContent(uwr);
+            audioSource.clip = audioClip;
+            //audioSource.isReadyToPLay();
+            Debug.Log("File Ready");
+        }
+    }
+
+    public void PlayAudioFile()
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        audioSource.loop = true;
+        Debug.Log("Play file");
+    }
+
+}
+
