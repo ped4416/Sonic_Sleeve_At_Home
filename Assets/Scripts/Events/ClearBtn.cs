23169810761347d8c98bb746d1308132bc29ccfa
ped4416 <ped4416@gmail.com>
2020-11-13 00:06:49 +0000

    fixed slider bugs and added new button images and music controls

---
+++ Assets/Scripts/Events/ClearBtn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearBtn : MonoBehaviour
+{
+
+    public void Click()
+    {
+        GetComponent<Image>().color = Color.red;
+        Debug.Log("Clear Button Pressed");
+    }
+}
---
+++ Assets/Scripts/Events/RecordBtn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RecordBtn : MonoBehaviour
+{
+    public Text mytext = null;
+    public int counter = 0;
+    public void changeText()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            mytext.color = Color.red;
+            mytext.text = "O";
+        }
+        else
+        {
+            mytext.color = Color.black;
+            mytext.text = "X";
+        }
+    }
+}
