854a9c7c8d8fc03568c8b8b3151ccb2cacd403b0
ped4416 <ped4416@gmail.com>
2020-11-21 11:16:14 +0000

    added stop run text script

---
+++ Assets/RunStopChangeText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RunStopChangeText : MonoBehaviour
+{
+    public TextMeshProUGUI mytext;
+    //public mytext = GetComponent<TextMeshPro>();
+    public int counter = 0;
+    public void changeText()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            mytext.color = Color.red;
+            mytext.text = "Stop";
+        }
+        else
+        {
+            mytext.color = Color.black;
+            mytext.text = "Run";
+        }
+    }
+}
