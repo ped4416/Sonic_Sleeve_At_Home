ff07844237ccd3316e7606a0709f7d6f17390eb2
Bryan Dunphy <bryandunphy@gmail.com>
2020-12-10 13:20:34 +0000

    gained access to interactML nodes through IMLComponent.cs. Connected GUI record button

---
+++ Assets/MLPNodeAccess.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+using InteractML.DataTypeNodes;
+
+public class MLPNodeAccess : MonoBehaviour
+{
+    private IMLConfiguration configNode;
+    private TrainingExamplesNode trainingNode;
+
+    public void MLPRecord()
+    {
+        trainingNode = gameObject.GetComponent<IMLComponent>().TrainingExamplesNodesList[0];
+        trainingNode.ToggleCollectExamples();
+    }
+
+    public void MLPTrain()
+    {
+        configNode = gameObject.GetComponent<IMLComponent>().IMLConfigurationNodesList[0];
+        configNode.TrainModel();
+    }
+
+    public void MLPRun()
+    {
+        configNode = gameObject.GetComponent<IMLComponent>().IMLConfigurationNodesList[0];
+        configNode.ToggleRunning();
+    }
+
+    public void MLPTargetValue(float targetVal)
+    {
+        trainingNode = gameObject.GetComponent<IMLComponent>().TrainingExamplesNodesList[0];
+        FloatNode floatNode = (FloatNode)trainingNode.TargetValues[0];
+        floatNode.m_UserInput = targetVal;
+    }
+
+    public void MLPDeleteTrainingExamples()
+    {
+        trainingNode = gameObject.GetComponent<IMLComponent>().TrainingExamplesNodesList[0];
+        trainingNode.ClearTrainingExamples();
+    }
+
+    public void MLPResetModel()
+    {
+        configNode = gameObject.GetComponent<IMLComponent>().IMLConfigurationNodesList[0];
+        configNode.ResetModel();
+    }
+}
--- Assets/Scripts/IML/IMLtoGUI.cs
+++ Assets/Scripts/IML/IMLtoGUI.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class IMLtoGUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject knnRight;
+    public GameObject knnLeft;
+    public GameObject shoulderAbductionRight;
+    public GameObject shoulderAbductionLeft;
+    public GameObject shoulderElevationRight;
+    public GameObject shoulderElevationLeft;
+    public GameObject trunkLeaning;
+    public GameObject armDropdown;
+    public GameObject knnEnableRecordButton;
+    public GameObject elbowEnableRecordButton;
+    public GameObject trunkEnableRecordButton;
+    public GameObject shoulderEnableRecordButton;
+
+    private int i_arm;
+    private bool knnOn;
+    private bool elbowOn;
+    private bool shoulderOn;
+    private bool trunkOn;
 
     // Update is called once per frame
     void Update()
     {
-        
+        i_arm = armDropdown.GetComponent<TMP_Dropdown>().value;
+        knnOn = knnEnableRecordButton.GetComponent<ToggleSwitch>().bOnOffSwitch;
+        elbowOn = elbowEnableRecordButton.GetComponent<ToggleSwitch>().bOnOffSwitch;
+        shoulderOn = shoulderEnableRecordButton.GetComponent<ToggleSwitch>().bOnOffSwitch;
+        trunkOn = trunkEnableRecordButton.GetComponent<ToggleSwitch>().bOnOffSwitch;
+    }
+
+    public void RecordData()
+    {
+        if(i_arm == 0)
+        {
+            if (knnOn) knnRight.GetComponent<KNNNodeAccess>().KNNRecord();
+            if (elbowOn) shoulderAbductionRight.GetComponent<MLPNodeAccess>().MLPRecord();
+            if (shoulderOn) shoulderElevationRight.GetComponent<MLPNodeAccess>().MLPRecord();
+        }
+        else if(i_arm == 1)
+        {
+            if (knnOn) knnLeft.GetComponent<KNNNodeAccess>().KNNRecord();
+            if (elbowOn) shoulderAbductionLeft.GetComponent<MLPNodeAccess>().MLPRecord();
+            if (shoulderOn) shoulderElevationLeft.GetComponent<MLPNodeAccess>().MLPRecord();
+        }
+
+        if (trunkOn) trunkLeaning.GetComponent<MLPNodeAccess>().MLPRecord();
     }
 }
