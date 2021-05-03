339fd1ff747c0c55d591f750336869fac4ea582c
pedro <ped4416@gmail.com>
2021-05-03 22:41:14 +0100

    Added data headers and output variables for shoulders, elbows aand wrists. Removed _raw ends of cariables to allow smoothed data only to be output to text file.

--- sonic_sleeve_2019_1_2/Assets/Scripts/ScriptableVariables/DataTracker.cs
+++ sonic_sleeve_2019_1_2/Assets/Scripts/ScriptableVariables/DataTracker.cs
@@ -6,9 +6,22 @@ using UnityEngine;
 public class DataTracker : ScriptableObject
 {
     public double rep_ms;
-    public double rep_error_ms;
+    public double rep_error_ms; 
+    // consider adding in smoothed data not just raw data as below
     public Vector3 neck_pos;
     public Quaternion neck_rot;
+    public Vector3 left_shoulder_pos;
+    public Quaternion left_shoulder_rot;
+    public Vector3 right_shoulder_pos;
+    public Quaternion right_shoulder_rot;
+    public Vector3 left_elbow_pos;
+    public Quaternion left_elbow_rot;
+    public Vector3 right_elbow_pos;
+    public Quaternion right_elbow_rot;
+    public Vector3 left_wrist_pos;
+    public Quaternion left_wrist_rot;
+    public Vector3 right_wrist_pos;
+    public Quaternion right_wrist_rot;
 
     public enum Condition
     { 
