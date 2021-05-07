4dd377fba811186539e18e9fbdfe8f7c1af854cf
pedro <ped4416@gmail.com>
2021-05-07 12:25:00 +0100

    updated shoulder, elbow and wrist variables in DataCollector & DataTracker. Fixed bug in start position text (missing object).

--- sonic_sleeve_2019_1_2/Assets/Scripts/DataClasses/DataCollector.cs
+++ sonic_sleeve_2019_1_2/Assets/Scripts/DataClasses/DataCollector.cs
@@ -41,12 +41,14 @@ public class DataCollector : MonoBehaviour
                 file.WriteLine("ID, date, timestamp, condition, elbow_comp, trunk_comp, shoulder_comp, pose_knn, cal_elbow, cal_trunk, cal_shoulder, block_n, rep_n, rep_ms, rep_error_ms, error_score, is_in_error, " +
                     "neck_x, neck_y, neck_z, " +
                     "shoulder_x, shoulder_y, shoulder_z, " +
-                    "left_elbow_x, left_elbow_y, left_elbow_z, left_elbow_quart_w, left_elbow_quart_x, left_elbow_quart_y, left_elbow_quart_z, " +
-                    "right_elbow_x, right_elbow_y, right_elbow_z, right_elbow_quart_w, right_elbow_quart_x, right_elbow_quart_y, right_elbow_quart_z, " +
-                    "left_wrist_x, left_wrist_y, left_wrist_z, left_wrist_quart_w_raw, left_wrist_quart_x_raw, left_wrist_quart_y_raw, left_wrist_quart_z, " +
-                    "right_wrist_x, right_wrist_y, right_wrist_z, right_wrist_quart_w, right_wrist_quart_x, right_wrist_quart_y, right_wrist_quart_z," +
+                    "elbow_x, elbow_y, elbow_z, " +
+                    "elbow_x, elbow_y, elbow_z, " +
+                    "wrist_x, wrist_y, wrist_z, " +
                     "neck_x_raw, neck_y_raw, neck_z_raw, neck_quart_w_raw, neck_quart_x_raw, neck_quart_y_raw, neck_quart_z_raw, " +
-                    "shoulder_x_raw, shoulder_y_raw, shoulder_z_raw, shoulder_quat_w_raw, shoulder_quat_x_raw, shoulder_quat_y_raw, shoulder_quat_z_raw");
+                    "shoulder_x_raw, shoulder_y_raw, shoulder_z_raw, shoulder_quat_w_raw, shoulder_quat_x_raw, shoulder_quat_y_raw, shoulder_quat_z_raw" +
+                    "elbow_x_raw, elbow_y_raw, elbow_z_raw, elbow_quart_w_raw, elbow_quart_x_raw, elbow_quart_y_raw, elbow_quart_z_raw, " +
+                    "wrist_x_raw, wrist_y_raw, wrist_z_raw, wrist_quat_w_raw, wrist_quat_x_raw, wrist_quat_y_raw, wrist_quat_z_raw"
+                    );
             }
         }
         catch (Exception ex)
@@ -55,15 +57,17 @@ public class DataCollector : MonoBehaviour
         }
     }
 
-    void AddData(string id, string date, string time, string condition, float elbow_comp, float trunk_comp, float shoulder_comp, int pose_knn, float cal_elbow, float cal_trunk, float cal_shoulder, int block_n, int rep_n, double rep_ms, double rep_error_ms, bool is_in_error, 
-                float neck_x, float neck_y, float neck_z, 
+    void AddData(string id, string date, string time, string condition, float elbow_comp, float trunk_comp, float shoulder_comp, 
+                int pose_knn, float cal_elbow, float cal_trunk, float cal_shoulder, int block_n, int rep_n, double rep_ms, double rep_error_ms, bool is_in_error,
+                float neck_x, float neck_y, float neck_z, //with low pass filter
                 float shoulder_x, float shoulder_y, float shoulder_z,
-                float left_elbow_x, float left_elbow_y, float left_elbow_z, float left_elbow_quart_w, float left_elbow_quart_x, float left_elbow_quart_y, float left_elbow_quart_z,
-                float right_elbow_x, float right_elbow_y, float right_elbow_z, float right_elbow_quart_w, float right_elbow_quart_x, float right_elbow_quart_y, float right_elbow_quart_z,
-                float left_wrist_x, float left_wrist_y, float left_wrist_z, float left_wrist_quart_w, float left_wrist_quart_x, float left_wrist_quart_y, float left_wrist_quart_z,
-                float right_wrist_x, float right_wrist_y, float right_wrist_z, float right_wrist_quart_w, float right_wrist_quart_x, float right_wrist_quart_y, float right_wrist_quart_z,
+                float elbow_x, float elbow_y, float elbow_z, 
+                float wrist_x, float wrist_y, float wrist_z, 
                 float neck_x_raw, float neck_y_raw, float neck_z_raw, float neck_quart_w_raw, float neck_quart_x_raw, float neck_quart_y_raw, float neck_quart_z_raw,
-                float shoulder_x_raw, float shoulder_y_raw, float shoulder_z_raw, float shoulder_quat_w_raw, float shoulder_quat_x_raw, float shoulder_quat_y_raw, float shoulder_quat_z_raw)
+                float shoulder_x_raw, float shoulder_y_raw, float shoulder_z_raw, float shoulder_quat_w_raw, float shoulder_quat_x_raw, float shoulder_quat_y_raw, float shoulder_quat_z_raw,
+                float elbow_x_raw, float elbow_y_raw, float elbow_z_raw, float elbow_quart_w_raw, float elbow_quart_x_raw, float elbow_quart_y_raw, float elbow_quart_z_raw,
+                float wrist_x_raw, float wrist_y_raw, float wrist_z_raw, float wrist_quat_w_raw, float wrist_quat_x_raw, float wrist_quat_y_raw, float wrist_quat_z_raw
+                )
     {     
         try
         {
@@ -72,12 +76,12 @@ public class DataCollector : MonoBehaviour
                 file.WriteLine(id + ", " + date + ", " + time + ", " + condition + ", " + elbow_comp + ", " + trunk_comp + ", " + shoulder_comp + ", " + pose_knn + ", " + cal_elbow + ", " + cal_trunk + ", " + cal_shoulder + ", " + block_n + ", " + rep_n + ", " + rep_ms + ", " + rep_error_ms + ", " + is_in_error + ", " + 
                     neck_x + ", " + neck_y + ", " + neck_z + ", " + 
                     shoulder_x + ", " + shoulder_y + ", " + shoulder_z + ", " + 
-                    left_elbow_x + ", " + left_elbow_y + ", " + left_elbow_z + ", " + left_elbow_quart_w + ", " + left_elbow_quart_x + ", " + left_elbow_quart_y + ", " + left_elbow_quart_z + ", " +
-                    right_elbow_x + ", " + right_elbow_y + ", " + right_elbow_z + ", " + right_elbow_quart_w + ", " + right_elbow_quart_x + ", " + right_elbow_quart_y + ", " + right_elbow_quart_z + ", " +
-                    left_wrist_x + ", " + left_wrist_y + ", " + left_wrist_z + ", " + left_wrist_quart_w + ", " + left_wrist_quart_x + ", " + left_wrist_quart_y + ", " + left_wrist_quart_z + ", " +
-                    right_wrist_x + ", " + right_wrist_y + ", " + right_wrist_z + ", " + right_wrist_quart_w + ", " + right_wrist_quart_x + ", " + right_wrist_quart_y + ", " + right_wrist_quart_z + ", " +
+                    elbow_x + ", " + elbow_y + ", " + elbow_z + ", " +
+                    wrist_x + ", " + wrist_y + ", " + wrist_z + ", " +
                     neck_x_raw + ", " + neck_y_raw + ", " + neck_z_raw + ", " + neck_quart_w_raw + ", " + neck_quart_x_raw + ", " + neck_quart_y_raw + ", " + neck_quart_z_raw + ", " +
-                    shoulder_x_raw + ", " + shoulder_y_raw + ", " + shoulder_z_raw + ", " + shoulder_quat_w_raw + ", " + shoulder_quat_x_raw + ", " + shoulder_quat_y_raw + ", " + shoulder_quat_z_raw
+                    shoulder_x_raw + ", " + shoulder_y_raw + ", " + shoulder_z_raw + ", " + shoulder_quat_w_raw + ", " + shoulder_quat_x_raw + ", " + shoulder_quat_y_raw + ", " + shoulder_quat_z_raw + ", " +
+                    elbow_x_raw + ", " + elbow_y_raw + ", " + elbow_z_raw + ", " + elbow_quart_w_raw + ", " + elbow_quart_x_raw + ", " + elbow_quart_y_raw + ", " + elbow_quart_z_raw + ", " +
+                    wrist_x_raw + ", " + wrist_y_raw + ", " + wrist_z_raw + ", " + wrist_quat_w_raw + ", " + wrist_quat_x_raw + ", " + wrist_quat_y_raw + ", " + wrist_quat_z_raw
                     );
             }
         }
@@ -92,15 +96,16 @@ public class DataCollector : MonoBehaviour
         currentTime = GetTime();
         currentDate = GetDate();
 
-        AddData(dataTracker.ID, currentDate, currentTime, dataTracker.e_condition.ToString(), dataTracker.elbow_comp, dataTracker.trunk_comp, dataTracker.shoulder_comp, dataTracker.pose_knn, dataTracker.cal_elbow, dataTracker.cal_trunk, dataTracker.cal_shoulder, dataTracker.block_n, dataTracker.rep_n, dataTracker.rep_ms, dataTracker.rep_error_ms, dataTracker.is_in_error, 
+        AddData(dataTracker.ID, currentDate, currentTime, dataTracker.e_condition.ToString(), dataTracker.elbow_comp, dataTracker.trunk_comp, dataTracker.shoulder_comp, dataTracker.pose_knn, 
+            dataTracker.cal_elbow, dataTracker.cal_trunk, dataTracker.cal_shoulder, dataTracker.block_n, dataTracker.rep_n, dataTracker.rep_ms, dataTracker.rep_error_ms, dataTracker.is_in_error, 
             dataTracker.neck_pos.x, dataTracker.neck_pos.y, dataTracker.neck_pos.z,
             dataTracker.shoulder_pos.x, dataTracker.shoulder_pos.y, dataTracker.shoulder_pos.z,
-            dataTracker.left_elbow_pos.x, dataTracker.left_elbow_pos.y, dataTracker.left_elbow_pos.z, dataTracker.left_elbow_rot.w, dataTracker.left_elbow_rot.x, dataTracker.left_elbow_rot.y, dataTracker.left_elbow_rot.z,
-            dataTracker.right_elbow_pos.x, dataTracker.right_elbow_pos.y, dataTracker.right_elbow_pos.z, dataTracker.right_elbow_rot.w, dataTracker.right_elbow_rot.x, dataTracker.right_elbow_rot.y, dataTracker.right_elbow_rot.z,
-            dataTracker.left_wrist_pos.x, dataTracker.left_wrist_pos.y, dataTracker.left_wrist_pos.z, dataTracker.left_wrist_rot.w, dataTracker.left_wrist_rot.x, dataTracker.left_wrist_rot.y, dataTracker.left_wrist_rot.z,
-            dataTracker.right_wrist_pos.x, dataTracker.right_wrist_pos.y, dataTracker.right_wrist_pos.z, dataTracker.right_wrist_rot.w, dataTracker.right_wrist_rot.x, dataTracker.right_wrist_rot.y, dataTracker.right_wrist_rot.z,
+            dataTracker.elbow_pos.x, dataTracker.elbow_pos.y, dataTracker.elbow_pos.z,
+            dataTracker.wrist_pos.x, dataTracker.wrist_pos.y, dataTracker.wrist_pos.z,
             dataTracker.neck_pos_raw.x, dataTracker.neck_pos_raw.y, dataTracker.neck_pos_raw.z, dataTracker.neck_rot_raw.w, dataTracker.neck_rot_raw.x, dataTracker.neck_rot_raw.y, dataTracker.neck_rot_raw.z,
-            dataTracker.shoulder_pos_raw.x, dataTracker.shoulder_pos_raw.y, dataTracker.shoulder_pos_raw.z, dataTracker.shoulder_rot_raw.w, dataTracker.shoulder_rot_raw.x, dataTracker.shoulder_rot_raw.y, dataTracker.shoulder_rot_raw.z
+            dataTracker.shoulder_pos_raw.x, dataTracker.shoulder_pos_raw.y, dataTracker.shoulder_pos_raw.z, dataTracker.shoulder_rot_raw.w, dataTracker.shoulder_rot_raw.x, dataTracker.shoulder_rot_raw.y, dataTracker.shoulder_rot_raw.z,
+            dataTracker.elbow_pos_raw.x, dataTracker.elbow_pos_raw.y, dataTracker.elbow_pos_raw.z, dataTracker.elbow_rot_raw.w, dataTracker.elbow_rot_raw.x, dataTracker.elbow_rot_raw.y, dataTracker.elbow_rot_raw.z,
+            dataTracker.wrist_pos_raw.x, dataTracker.wrist_pos_raw.y, dataTracker.wrist_pos_raw.z, dataTracker.wrist_rot_raw.w, dataTracker.wrist_rot_raw.x, dataTracker.wrist_rot_raw.y, dataTracker.wrist_rot_raw.z
             );
         //AddData("", "", dataTracker.e_condition.ToString(), dataTracker.rep_n, dataTracker.rep_ms, dataTracker.is_in_error, dataTracker.rep_error_ms, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
     }
--- sonic_sleeve_2019_1_2/Assets/Scripts/ScriptableVariables/DataTracker.cs
+++ sonic_sleeve_2019_1_2/Assets/Scripts/ScriptableVariables/DataTracker.cs
@@ -7,29 +7,21 @@ public class DataTracker : ScriptableObject
 {
     public double rep_ms;
     public double rep_error_ms; 
-    // consider adding in smoothed data not just raw data as below
+    // smoothed data used prior to auditory feedback 
     public Vector3 neck_pos;
+    public Vector3 shoulder_pos;
+    public Vector3 elbow_pos;
+    public Vector3 wrist_pos;
     //public Quaternion neck_rot;
+    //all raw data 3D and quaternians
     public Vector3 neck_pos_raw;
     public Quaternion neck_rot_raw;
-    public Vector3 shoulder_pos;
     public Vector3 shoulder_pos_raw;
     public Quaternion shoulder_rot_raw;
-    public Vector3 elbow_pos;
     public Vector3 elbow_pos_raw;
     public Quaternion elbow_rot_raw;
-    public Vector3 left_elbow_pos;
-    public Quaternion left_elbow_rot;
-    public Vector3 right_elbow_pos;
-    public Quaternion right_elbow_rot;
-    public Vector3 wrist_pos;
     public Vector3 wrist_pos_raw;
     public Quaternion wrist_rot_raw;
-    public Vector3 left_wrist_pos;
-    public Quaternion left_wrist_rot;
-    public Vector3 right_wrist_pos;
-    public Quaternion right_wrist_rot;
-
     public enum Condition
     { 
         Control,
