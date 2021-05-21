380a82667ee9ae0183ef1ce25470fbea377aac47
pedro <ped4416@gmail.com>
2021-05-21 10:14:43 +0100

    sorted target collision detector - needed to use OnTriggerStay and add a tag to the wrist position (Player)

--- sonic_sleeve_2019_1_2/Assets/Scripts/Automation/ChangeBoxColour.cs
+++ sonic_sleeve_2019_1_2/Assets/Scripts/Automation/ChangeBoxColour.cs
@@ -13,24 +13,35 @@ public class ChangeBoxColour : MonoBehaviour
         col = Color.green;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    //private void OnCollisionEnter(Collision collision) //bug with physics iterfering. try trigger instead 
+    private void OnTriggerStay(Collider other) //can use OnTriggerEnter so it runs only once rather than Stay 
     {
-        print("HITTTT");
-        StartCoroutine(ColourChangeCoroutine());
+
+    
+        if (other.CompareTag("Player"))
+        {
+            print("HITTTT");
+            //StartCoroutine(ColourChangeCoroutine());
+            mat = gameObject.GetComponent<Renderer>().material;
+            mat.color = Color.green;
+        }
     }
 
-    IEnumerator ColourChangeCoroutine()
+    /*IEnumerator ColourChangeCoroutine()
     {
         mat = gameObject.GetComponent<Renderer>().material;
         mat.color = col;
         yield return new WaitForSeconds(1.0f);
         mat.color = Color.white;
-    }    
-
-    /*private void OnCollisionExit(Collision collision)
-    {
-        print("EXIT");
-        mat = gameObject.GetComponent<Renderer>().material;
-        mat.color = Color.white;
     }*/
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("EXIT");
+            mat = gameObject.GetComponent<Renderer>().material;
+            mat.color = Color.white;
+        }
+    }
 }
